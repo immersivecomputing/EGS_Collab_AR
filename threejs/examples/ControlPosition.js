08380c3905cca61a27f0513eb0abc890ba1c02ef
prat615 <martin.pratt@pnnl.gov>
2021-10-06 12:21:41 -0700

    switch to function instead

--- threejs/examples/ControlPosition.js
+++ threejs/examples/ControlPosition.js
@@ -1,26 +1,24 @@
-class ControlPosition {
+
 	
-	SetPosition(){
-		let positionAlongBorehole = getText();
-		console.log(positionAlongBorehole);
-	}
-	
-	getText(){
-		var request = new XMLHttpRequest();
-		request.open('GET', 'https://raw.githubusercontent.com/immersivecomputing/EGS_Collab_AR/main/TextFile/Position.txt', true);
-		request.send(null);
-		request.onreadystatechange = function () {
-			if (request.readyState === 4 && request.status === 200) {
-				var type = request.getResponseHeader('Content-Type');
-				if (type.indexOf("text") !== 1) {
-					return request.responseText;
-				}
+function ControlPosition(){
+	let positionAlongBorehole = getText();
+	console.log(positionAlongBorehole);
+}
+
+function getText(){
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://raw.githubusercontent.com/immersivecomputing/EGS_Collab_AR/main/TextFile/Position.txt', true);
+	request.send(null);
+	request.onreadystatechange = function () {
+		if (request.readyState === 4 && request.status === 200) {
+			var type = request.getResponseHeader('Content-Type');
+			if (type.indexOf("text") !== 1) {
+				return request.responseText;
 			}
 		}
 	}
-	
 }
+	
 
-ControlPosition.prototype.isControlPosition = true;
 
 export ( ControlPosition );
