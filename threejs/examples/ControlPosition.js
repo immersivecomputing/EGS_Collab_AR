dfabe1dec6ab988ea92bcbe63d662009cc39bf7d
prat615 <martin.pratt@pnnl.gov>
2021-10-06 12:28:56 -0700

    fix braces

--- threejs/examples/ControlPosition.js
+++ threejs/examples/ControlPosition.js
@@ -1,24 +1,25 @@
-
+class ControlPosition {
 	
-function ControlPosition(){
-	let positionAlongBorehole = getText();
-	console.log(positionAlongBorehole);
-}
 
-function getText(){
-	var request = new XMLHttpRequest();
-	request.open('GET', 'https://raw.githubusercontent.com/immersivecomputing/EGS_Collab_AR/main/TextFile/Position.txt', true);
-	request.send(null);
-	request.onreadystatechange = function () {
-		if (request.readyState === 4 && request.status === 200) {
-			var type = request.getResponseHeader('Content-Type');
-			if (type.indexOf("text") !== 1) {
-				return request.responseText;
+	setPosition(){
+		let positionAlongBorehole = getText();
+		console.log(positionAlongBorehole);
+	}
+	
+	getText(){
+		var request = new XMLHttpRequest();
+		request.open('GET', 'https://raw.githubusercontent.com/immersivecomputing/EGS_Collab_AR/main/TextFile/Position.txt', true);
+		request.send(null);
+		request.onreadystatechange = function () {
+			if (request.readyState === 4 && request.status === 200) {
+				var type = request.getResponseHeader('Content-Type');
+				if (type.indexOf("text") !== 1) {
+					return request.responseText;
+				}
 			}
 		}
 	}
-}
 	
+}
 
-
-export ( ControlPosition );
+export { ControlPosition };
