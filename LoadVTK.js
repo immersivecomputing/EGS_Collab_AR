00c3d330b47d516c594330a20ff8d8a1a4dbc5d5
prat615 <martin.pratt@pnnl.gov>
2022-02-10 14:46:25 -0800

    set the scale and position

--- LoadVTK.js
+++ LoadVTK.js
@@ -1,13 +1,24 @@
 import { VTKLoader } from './threejs/examples/jsm/loaders/VTKLoader.js';
 import * as THREE from './threejs/build/three.module.js';
 
-function LoadVTK(fileName, scene){
+function LoadVTK(fileName, scene, redScale, convert){
     const loader1 = new VTKLoader();
     loader1.load(fileName, function (geometry){
         geometry.computeVertexNormals();
 
         const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
         const mesh = new THREE.Mesh( geometry, material );
+
+        mesh.position.set(4047.435, -1047.088, -2779.125);
+
+        if(convert){
+            mesh.scale.set(3.28084 * redScale, 3.28084 * redScale, 3.28084 * redScale);
+        }
+        else{
+            mesh.scale.set(redScale, redScale, redScale);
+        }
+
+        
         scene.add(mesh)
 
     })
