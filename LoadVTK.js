20346bf2c1d8ddfac002ff32bea6a145a54287f0
prat615 <martin.pratt@pnnl.gov>
2022-02-10 14:12:13 -0800

    add the three module

--- LoadVTK.js
+++ LoadVTK.js
@@ -1,4 +1,5 @@
 import { VTKLoader } from './threejs/examples/jsm/loaders/VTKLoader.js';
+import * as THREE from './threejs/build/three.module.js';
 
 function LoadVTK(fileName, scene){
     const loader1 = new VTKLoader();
