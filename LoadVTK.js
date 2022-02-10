78b50728af456ef1fbdf60c7e669853e729d41e7
prat615 <martin.pratt@pnnl.gov>
2022-02-10 15:47:49 -0800

    try again

--- LoadVTK.js
+++ LoadVTK.js
@@ -1,7 +1,7 @@
 import { VTKLoader } from './threejs/examples/jsm/loaders/VTKLoader.js';
 import * as THREE from './threejs/build/three.module.js';
 
-function LoadVTK(fileName, scene, convert){
+function LoadVTK(fileName, group, convert){
     const loader1 = new VTKLoader();
     loader1.load(fileName, function (geometry){
         geometry.computeVertexNormals();
@@ -9,15 +9,14 @@ function LoadVTK(fileName, scene, convert){
         const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
         const mesh = new THREE.Mesh( geometry, material );
 
-        
-
         if(convert){
-            mesh.scale.set(3.28084, 3.28084, 3.28084);
+            mesh.scale.set(3.28084 * 0.01, 3.28084 * 0.01, 3.28084 * 0.01);
         }
 
+        group.add(mesh)
         mesh.position.set(4047.435, -1047.088, 2779.125);
         
-        scene.add(mesh)
+        
 
     })
 }
