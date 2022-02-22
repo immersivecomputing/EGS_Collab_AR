05a3e96182e484a41f006709e4210606599273a1
prat615 <martin.pratt@pnnl.gov>
2022-02-22 08:55:18 -0800

    try adding some extra text to the page

---
+++ FeedbackText.js
@@ -0,0 +1,24 @@
+const feedback = document.createElement( 'FeedbackText' );
+
+
+function ShowFeedBackText(text){
+    feedback.style.display = '';
+
+	feedback.style.cursor = 'pointer';
+	feedback.style.left = 'calc(50% - 50px)';
+	feedback.style.width = '100px';
+    feedback.textContent = text;
+    feedback.style.bottom = '20px';
+    feedback.style.padding = '12px 6px';
+    feedback.style.border = '1px solid #fff';
+    feedback.style.borderRadius = '4px';
+    feedback.style.background = 'rgba(0,0,0,0.1)';
+    feedback.style.color = '#fff';
+    feedback.style.font = 'normal 13px sans-serif';
+    feedback.style.textAlign = 'center';
+    feedback.style.opacity = '0.5';
+    feedback.style.outline = 'none';
+	feedback.style.zIndex = '999';
+}
+
+export {ShowFeedBackText};
