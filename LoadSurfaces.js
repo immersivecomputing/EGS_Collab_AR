771ef893f91b75f86dd6faf47de306b062fd89c6
prat615 <martin.pratt@pnnl.gov>
2022-06-13 11:15:42 -0700

    fix up load vtk

--- LoadSurfaces.js
+++ LoadSurfaces.js
@@ -2,25 +2,49 @@ import { VTKLoader } from './threejs/examples/jsm/loaders/VTKLoader.js';
 import { OBJLoader } from './threejs/examples/jsm/loaders/OBJLoader.js';
 import * as THREE from './threejs/build/three.module.js';
 
-function LoadVTK(fileName, group, convert){
-    const loader1 = new VTKLoader();
-    loader1.load(fileName, function (geometry){
+// function LoadVTK(fileName, group, convert, renderOrder, surfColor){
+//     const loader1 = new VTKLoader();
+//     loader1.load(fileName, function (geometry){
+//         geometry.computeVertexNormals();
+
+//         const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+//         const mesh = new THREE.Mesh( geometry, material );
+
+//         if(convert){
+//             mesh.scale.set(3.28084, 3.28084, 3.28084);
+//         }
+
+//         mesh.position.set(-4047.435, -1047.088, -2779.125);
+//         mesh.rotation.set(-90 * (Math.PI / 180), 0 ,0);
+
+//         group.add(mesh);
+
+//     })
+// }
+
+
+function LoadVTK(loadedModel, VTKfilePath, renderOrder, surfColor){
+    const loader = new VTKLoader();
+    loader.load( '/EGS_Collab_AR/ERT_Data/' + VTKfilePath, function ( geometry ) {
+
         geometry.computeVertexNormals();
+        const material = new THREE.MeshLambertMaterial( { 
+            color: surfColor,
+            opacity: 0.1,
+            transparent: true,
+            side: 2
+        });
 
-        const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-        const mesh = new THREE.Mesh( geometry, material );
-
-        if(convert){
-            mesh.scale.set(3.28084, 3.28084, 3.28084);
-        }
-
-        mesh.position.set(-4047.435, -1047.088, -2779.125);
-        mesh.rotation.set(-90 * (Math.PI / 180), 0 ,0);
-
-        group.add(mesh);
-
-    })
+        const mesh = new THREE.Mesh( geometry, material);
+        mesh.renderOrder = renderOrder;
+        loadedModel.add( mesh );
+    });
 }
+
+
+
+
+
 
 function LoadOBJ(fileName, group, convert, renderOrder, surfColor){
     const loader1 = new OBJLoader();
