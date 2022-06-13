51a167ab7205e0aff6cbc6ac5f2cacb98702b8a8
prat615 <martin.pratt@pnnl.gov>
2022-06-13 11:28:24 -0700

    flip render order

--- LoadSurfaces.js
+++ LoadSurfaces.js
@@ -2,25 +2,6 @@ import { VTKLoader } from './threejs/examples/jsm/loaders/VTKLoader.js';
 import { OBJLoader } from './threejs/examples/jsm/loaders/OBJLoader.js';
 import * as THREE from './threejs/build/three.module.js';
 
-// function LoadVTK(fileName, group, convert, renderOrder, surfColor){
-//     const loader1 = new VTKLoader();
-//     loader1.load(fileName, function (geometry){
-//         geometry.computeVertexNormals();
-
-//         const material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
-//         const mesh = new THREE.Mesh( geometry, material );
-
-//         if(convert){
-//             mesh.scale.set(3.28084, 3.28084, 3.28084);
-//         }
-
-//         mesh.position.set(-4047.435, -1047.088, -2779.125);
-//         mesh.rotation.set(-90 * (Math.PI / 180), 0 ,0);
-
-//         group.add(mesh);
-
-//     })
-// }
 
 
 function LoadVTK(loadedModel, VTKfilePath, renderOrder, surfColor){
