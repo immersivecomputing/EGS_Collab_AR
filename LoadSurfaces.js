6e3bc00ac2a5a36eeb3eb14bed65eb220aaea5d9
prat615 <martin.pratt@pnnl.gov>
2022-06-21 15:39:34 -0700

    try again

--- LoadSurfaces.js
+++ LoadSurfaces.js
@@ -15,7 +15,7 @@ import * as THREE from './threejs/build/three.module.js';
 
 function LoadVTK(loadedModel, VTKfilePath, renderOrder, surfColor, clipPlanes){
     const loader = new VTKLoader();
-    loader.load( pathPrefix + 'EGS_Collab_AR/ERT_Data/' + VTKfilePath, function ( geometry ) {
+    loader.load( pathPrefix + 'ERT_Data/' + VTKfilePath, function ( geometry ) {
 
         geometry.computeVertexNormals();
         const material = new THREE.MeshLambertMaterial( { 
